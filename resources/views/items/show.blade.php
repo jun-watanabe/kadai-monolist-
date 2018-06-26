714f9410db02451ebdcaa14a4f1c2d308692c884
jun.watanabe <jun.b.watanabe@rakuten.com>
2018-06-26 00:50:16 +0000

    Procfile

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -40,6 +40,11 @@
                         Haveしたユーザ
                     </div>
                     <div class="panel-body">
+                        @foreach ($have_users as $user)
+                            <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
+                        @endforeach
+                    </div>    
+                        
                     </div>
                 </div>
             </div>
